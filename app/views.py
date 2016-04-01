499ae9b6c6472fa68a92ffcdf334f4781c65b2e6
Elizaveta Kuzmenko <lizaku77@gmail.com>
2016-04-02 01:15:27 +0300

    queries to elastic and sqlite

--- app/views.py
+++ app/views.py
@@ -2,25 +2,14 @@ from flask import render_template
 from flask import request
 from app import app
 import sqlite3
+from elasticsearch import Elasticsearch
+import re
+from collections import defaultdict, OrderedDict
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # выдуманный пользователь
-    posts = [  # список выдуманных постов
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("search.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    return render_template("search.html")
 
 @app.route('/sql_search', methods=['GET', 'POST'])
 def sql_search():
@@ -51,3 +40,32 @@ def sql_search():
     base.close()
     return render_template("search.html", title='Search', text=text, results=results, tags=tags,
                            tags_results=tags_results)
+
+@app.route('/elastic', methods=['GET', 'POST'])
+def elastic_search():
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    text = False
+    texts = False
+    try:
+        text = request.form['text_query']
+    except:
+        pass
+    if text:
+        query = es.search(index="corpus", body={"query": {"match": {'text': text}}})
+        results = query['hits']['hits']
+        texts = []
+        for r in results:
+            res = r['_source']['text']
+            #res = defaultdict(OrderedDict)
+            #q = r['_source']['text'].replace(text, '<em>' + text + '<em>')
+            #parts = q.split('<em>')
+            #for part in parts:
+            #    if part == text:
+            #        res[part] = 'yes'
+            #    else:
+            #        res[part] = 'no'
+            texts.append(res)
+    return render_template("elastic.html", texts=texts)
+
+def neo('/neo_search', methods=['GET', 'POST']):
+    return render_template("neo.html")
