2f6fb69b07687037cceafb02e7122d5568c38812
Elizaveta Kuzmenko <lizaku77@gmail.com>
2016-04-02 01:59:29 +0300

    simple queries work for all databases

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from flask import request
 from app import app
 import sqlite3
 from elasticsearch import Elasticsearch
-import re
-from collections import defaultdict, OrderedDict
+from neo4jrestclient import client
+from neo4jrestclient.client import GraphDatabase
+
+tags = ['Prepositional_verb', 'Double_object', 'Quant_choice', 'Conditionals', 'Prepositional_adv', 'Often_confused', 'No_diff', 'Quantifiers', 'Agreement_errors', 'Modals_form', 'Attributes', 'Choice_of_ref', 'Animacy', 'Absence_comp_colloc', 'Det_form', 'Causation', 'Voice_form', 'Note_structure', 'Modals', 'Pronouns', 'Demonstrative', 'Modals_choice', 'Comparative_adv', 'Word_order', 'Relative_clause', 'Determiners', 'Incoherent_pron', 'Incoherent_conj', 'Det_choice', 'Diff', 'Person', 'Punctuation', 'Abs_comp_clause', 'Redundant', 'Discourse', 'Choice_synonyms', 'Interrogative', 'Superlative_adj', 'Personal', 'Standard', 'Derivation', 'Formational_affixes', 'Verb_prep_Gerund', 'Redundant_ref', 'Adverbs', 'Lack_of_ref_device', 'Conjunctions', 'that_clause', 'Seq_of_tenses', 'Numerical', 'Double_prep_phrasal', 'suggestion', 'Verb_Gerund', 'lex_part_choice', 'Vocabulary', 'Dangling_ref', 'Concession', 'Voice', 'Comparative_adj', 'Adjectives', 'Title_structure', 'Verb_pattern', 'Participial_constr', 'Verbs', 'Infinitive_constr', 'Coherence', 'Tense_choice', 'Incoherent_articles', 'Trans_phrasal', 'Spelling', 'Transitive', 'Presentation', 'And_syn', 'Word_choice', 'Category_confusion', 'Defining', 'Comparative_constr', 'Modifier', 'Tense_form', 'Verb_patterns', 'Grammar', 'Non_defining', 'Dative', 'Verb_Inf', 'Tense', 'Voice_choice', 'Noun_number', 'Form_in_cond', 'Attr_participial', 'Incoherent_in_cond', 'Trans_prep', 'Countable_uncountable', 'Number', 'Adjective_inf', 'Lack_of_connective', 'Prepositional_noun', 'Articles', 'lex_item_choice', 'Absence_explanation', 'Negation', 'Verb_object_inf', 'Incoherent_intro_unit', 'Redundant_comp', 'Incoherent_tenses', 'Suffix', 'Prepositions', 'Num_form', 'Contrast', 'Art_choice', 'Possessive', 'Art_form', 'Absence_comp_sent', 'Inappropriate_register', 'Quant_form', 'Tautology', 'Emphatic', 'Nouns', 'Lack_par_constr', 'Prepositional_adjective', 'Collective']
 
 @app.route('/')
 @app.route('/index')
@@ -19,7 +21,6 @@ def sql_search():
     tag_value = False
     results = False
     tags_results = False
-    tags = ['Articles', 'Lex_item_choice', 'Prepositions']
     try:
         text = request.form['text_query']
     except:
@@ -34,7 +35,6 @@ def sql_search():
     if tag_value:
         tags_results = []
         for tag in tag_value:
-            #print(tag)
             c.execute('SELECT text FROM tags WHERE type=?', (tag,))
             tags_results += list(set([i[0] for i in c.fetchall()]))
     base.close()
@@ -67,5 +67,21 @@ def elastic_search():
             texts.append(res)
     return render_template("elastic.html", texts=texts)
 
-def neo('/neo_search', methods=['GET', 'POST']):
-    return render_template("neo.html")
+@app.route('/neo_search', methods=['GET', 'POST'])
+def neo():
+    db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
+    tag_value = False
+    res = []
+    try:
+        tag_value = request.form.getlist('tag_query')
+    except:
+        pass
+    if tag_value:
+        res = []
+        for tag in tag_value[:-1]:
+            query = 'MATCH (t:Texts)-[r:contains]->(m:Mistake) WHERE m.name="%s" RETURN t, type(r), m' % tag
+            results = db.query(query, returns=(client.Node, str, client.Node))
+            for r in results:
+                line = r[0]["name"] + ' ' + r[1] + ' ' + r[2]["name"]
+                res.append(line)
+    return render_template("neo.html", res=res, tags=tags)
--- realec_graphs.py
+++ realec_graphs.py
@@ -2,52 +2,54 @@ from neo4jrestclient import client
 from neo4jrestclient.client import GraphDatabase
 import json
 
+db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
 
+def create():
+    texts = db.labels.create("Texts")
+    # Create some nodes with labels
+    identifiers = {'texts':{}, 'mistakes': {}}
+    with open('./elastic/texts.json', 'r', encoding='utf-8') as f:
+        i = 0
+        for line in f:
+            print('texts', i)
+            data = json.loads(line.strip())
+            name = data['t_name']
+            text = data['text']
+            mark = data['essay_mark']
+            year = data['year']
+            fac = data['faculty']
+            t_type = data['task_type']
+            t = db.nodes.create(name=name, text=text, mark=mark, year=year, faculty=fac, type=t_type, t_id=i)
+            texts.add(t)
+            identifiers['texts'][i] = t.id
+            i += 1
 
-texts = db.labels.create("Texts")
-# Create some nodes with labels
-identifiers = {'texts':{}, 'mistakes': {}}
-with open('./elastic/texts.json', 'r', encoding='utf-8') as f:
-    i = 0
-    for line in f:
-        print('texts', i)
-        data = json.loads(line.strip())
-        name = data['t_name']
-        text = data['text']
-        mark = data['essay_mark']
-        year = data['year']
-        fac = data['faculty']
-        t_type = data['task_type']
-        t = db.nodes.create(name=name, text=text, mark=mark, year=year, faculty=fac, type=t_type, t_id=i)
-        texts.add(t)
-        identifiers['texts'][i] = t.id
-        i += 1
+    mistakes = {}
+    mist = db.labels.create("Mistake")
+    with open('./elastic/mistakes.json', 'r', encoding='utf-8') as f:
+        i = 0
+        for line in f:
+            print('mistakes', i)
+            data = json.loads(line.strip())
+            name = data['m_type']
+            if name not in mistakes:
+                m = db.nodes.create(name=name, id=i)
+                mist.add(m)
+                mistakes[name] = m.id
+            text = data['m_text']
+            corr = data['correction']
+            w_l = data['weight_language']
+            cause = data['cause']
+            t_id = int(data['t_id'])
+            db.node[identifiers['texts'][t_id]].relationships.create("contains", db.node[mistakes[name]], text=text,
+                                                                     correction=corr, weight_language=w_l, cause=cause)
+            i += 1
 
-mistakes = {}
-mist = db.labels.create("Mistake")
-with open('./elastic/mistakes.json', 'r', encoding='utf-8') as f:
-    i = 0
-    for line in f:
-        print('mistakes', i)
-        data = json.loads(line.strip())
-        name = data['m_type']
-        if name not in mistakes:
-            m = db.nodes.create(name=name, id=i)
-            mist.add(m)
-            mistakes[name] = m.id
-        text = data['m_text']
-        corr = data['correction']
-        w_l = data['weight_language']
-        cause = data['cause']
-        t_id = int(data['t_id'])
-        db.node[identifiers['texts'][t_id]].relationships.create("contains", db.node[mistakes[name]], text=text,
-                                                                 correction=corr, weight_language=w_l, cause=cause)
-        i += 1
+def query():
+    q = 'MATCH (t:Texts)-[r:contains]->(m:Mistake) WHERE m.name="%s" RETURN t, type(r), m' % 'Articles'
+    results = db.query(q, returns=(client.Node, str, client.Node))
+    for r in results:
+        print("(%s)-[%s]->(%s)" % (r[0]["name"], r[1], r[2]["name"]))
 
-
-
-#q = 'MATCH (u:Texts)-[r:contains]->(m:Mistakes) WHERE u.name="esl_0021" RETURN u, type(r), m'
-# "db" as defined above
-#results = db.query(q, returns=(client.Node, str, client.Node))
-#for r in results:
-#    print("(%s)-[%s]->(%s)" % (r[0]["name"], r[1], r[2]["name"]))
+if __name__ == '__main__':
+    query()
